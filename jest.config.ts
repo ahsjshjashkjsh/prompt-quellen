78dbc71a1df19236027aba996a512cd834055422
Development <development@local>
2026-03-30 17:55:44 +0200

    chore: add tsconfig paths and jest ts-jest config for middleware support

--- jest.config.ts
+++ jest.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/$1',
   },
   testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        ignoreDeprecations: '6.0',
+      },
+    },
+  },
 }
 
 export default config
